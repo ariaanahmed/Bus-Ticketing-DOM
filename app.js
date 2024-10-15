c9a55a7599d193f785c7676845596494e033b313
ariaanahmed <ariaanahmed24@gmail.com>
2024-10-15 12:43:51 +0600

    responsive navbar added

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('hello');
+function toggle() {
+    const smnav = document.getElementById('sm-nav-ul');
+
+    if(smnav.classList.contains('hidden')) {
+        smnav.classList.remove('hidden');
+    } else {
+        smnav.classList.add('hidden');
+    }
+}
